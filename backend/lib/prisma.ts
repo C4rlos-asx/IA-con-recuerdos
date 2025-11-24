10562bbbc13990fbfa76cd379c49ea0600db8373
C4rlos-asx <carlosmedinaalberto84@gmail.com>
2025-11-24 14:23:40 -0400

    fix prisma

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool } from '@neondatabase/serverless';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const neon = new Pool({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaNeon(neon);
+
 export const prisma =
     globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['query'],
-    });
+    new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
