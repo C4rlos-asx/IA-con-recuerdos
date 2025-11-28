2354d593d0a0c73b3e98e5fa44314a2ef5a844d6
C4rlos-asx <carlosmedinaalberto84@gmail.com>
2025-11-28 08:28:54 -0400

    fix modelos ia

--- backend/client.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="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")
-
-for model in client.models.list():
-
-    print(model)
